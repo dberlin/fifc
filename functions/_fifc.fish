f906234458571379a616546916d1ac1a58cf6e96
Daniel Berlin <dberlin@dberlin.org>
2024-11-04 12:11:38 -0500

    Don't ever add extra spaces

--- functions/_fifc.fish
+++ functions/_fifc.fish
@@ -50,7 +50,7 @@ function _fifc
     eval $cmd | while read -l token
         # don't escape '~' for path, `$` for environ
         if string match --quiet '~*' -- $token
-            set -a result (eval 'echo' (string join -- "" "~" (string sub --start 2 -- $token | string escape)))
+            set -a result (string join -- "" "~" (string sub --start 2 -- $token | string escape))
         else if string match --quiet '$*' -- $token
             set -a result (string join -- "" "\$" (string sub --start 2 -- $token | string escape))
         else
@@ -74,7 +74,7 @@ function _fifc
     end
 
     if test -n "$result"
-        commandline --replace --current-token -- (string join -- ' ' $result)
+        commandline --replace --current-token -- (string join -- '' $result)
     end
 
     commandline --function repaint
